ea90dae5534e255846df42c583ba109ac67b753f
Bodomrus70 <Bodomrus70@mail.ru>
2022-11-22 22:13:55 +0700

    Module 5.3

--- Module5/Module5.cs
+++ Module5/Module5.cs
@@ -1,62 +1,25 @@
 ﻿using System;
-
-class MainClass
+class Programm
 {
-
-    static void ShowColor()
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
-        var color = Console.ReadLine();
-        
-        switch (color)
-        {
-            case "red":
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.ForegroundColor = ConsoleColor.Black;
 
-                Console.WriteLine("Your color is red!");
-                break;
+        var someName = "Дмитрий";
+        Console.WriteLine(someName);
 
-            case "green":
-                Console.BackgroundColor = ConsoleColor.Green;
-                Console.ForegroundColor = ConsoleColor.Black;
+        GetName(someName);
 
-                Console.WriteLine("Your color is green!");
-                break;
-            case "cyan":
-                Console.BackgroundColor = ConsoleColor.Cyan;
-                Console.ForegroundColor = ConsoleColor.Black;
+        Console.WriteLine(someName);
 
-                Console.WriteLine("Your color is cyan!");
-                break;
-            default:
-                Console.BackgroundColor = ConsoleColor.Yellow;
-                Console.ForegroundColor = ConsoleColor.Red;
-
-                Console.WriteLine("Your color is yellow!");
-                break;
-        }
+        Console.ReadKey();
     }
 
-
-     static void Main(string[] args)
+    static void GetName(string name)
     {
-
-        var (name, age) = ("Евгения", 27);
-
-        Console.WriteLine("Мое имя: {0}", name);
-        Console.WriteLine("Мой возраст: {0}", age);
-
-        Console.Write("Введите имя: ");
+        Console.WriteLine("Введите имя");
+       
         name = Console.ReadLine();
-        Console.Write("Введите возрас с цифрами:");
-        age = Convert.ToInt32(Console.ReadLine());
-
-        Console.WriteLine("Ваше имя: {0}", name);
-        Console.WriteLine("Ваш возраст: {0}", age);
-
-        ShowColor();
-
 
     }
+
 }
