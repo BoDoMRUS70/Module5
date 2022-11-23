f73ab8140f1a458b93ff96c80be170fd07a43e7a
Bodomrus70 <Bodomrus70@mail.ru>
2022-11-23 21:36:30 +0700

    цветное ЭХО

--- Module5/Module5.cs
+++ Module5/Module5.cs
@@ -18,20 +18,18 @@ class Programm
     static void Echo(string saidworld, int deep)
     {
         var modif = saidworld;
+
+        if (modif.Length > 2)
         {
-            Console.WriteLine(saidworld);
+            modif = modif.Remove(0, 2);
+        }
 
-            if (modif.Length > 2)
-            {
-                modif = modif.Remove(0, 2);
-            }
+        Console.BackgroundColor = (ConsoleColor)deep;
+        Console.WriteLine("..." + modif);
 
-            Console.WriteLine("..." + modif);
-
-            if (deep > 1)
-            {
-                Echo(modif, deep - 1);
-            }
+        if (deep > 1)
+        {
+            Echo(modif, deep - 1);
         }
     }
 }
